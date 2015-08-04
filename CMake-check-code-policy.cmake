cec3b1fb060f66e13be3c66f18a0b57c3c06646c
Kenney Phillis Jr <kphillisjr@gmail.com>
2015-08-04 05:09:17 -0500

    cmake: Add Code Policy Check and automatic whitespace fix targets
    This adds two targets to cmake.
    
    autofix_whitespace - target that automatically fixes whitespace errors.
    
    codepolicycheck - Target that parses sources checking for policy errors.

---
+++ CMake-check-code-policy.cmake
@@ -0,0 +1,22 @@
+#TODO: Run Python Script.
+find_package(PythonInterp REQUIRED)
+
+
+
+# Add code policy check custom target.
+add_custom_target(autofix_whitespace
+	${PYTHON_EXECUTABLE}
+		"util/autofix_whitespace.py"
+		"-d" "src" "-d" "tests" "--ext" ".c" "--ext"  ".h" "--ext" ".h.in"
+	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}		
+	COMMENT "Verifying Code Policy")
+
+	
+# Add code policy check custom target.
+add_custom_target(codepolicycheck
+	${PYTHON_EXECUTABLE}
+		"util/check_code_policy.py"
+		"-d" "src" "-d" "tests" "--ext" ".c" "--ext"  ".h" "--ext" ".h.in"
+	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+	DEPENDS autofix_whitespace_issues
+	COMMENT "Verifying Code Policy")
